9bb345bee0ef2f3b6f30fdb948c0e7117449f697
Mateus Ferreira Canedo <75223788+fferreiracanedo@users.noreply.github.com>
2024-08-09 14:43:04 -0300

    Update PrimitiveToWrapperClass.java

--- src/br/com/devmateus/PrimitiveToWrapperClass.java
+++ src/br/com/devmateus/PrimitiveToWrapperClass.java
@@ -4,7 +4,7 @@ public class PrimitiveToWrapperClass {
 
     public void printResult(){
         int n1 = 10;
-        Integer n2 = 10;
+        Integer n2 = Integer.valueof(n1);
 
         System.out.println(n2);
     }
